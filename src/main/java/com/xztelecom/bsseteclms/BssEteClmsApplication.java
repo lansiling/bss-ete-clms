60cbee4296a463015be300bd2c926f0f38fddd7b
kevin <104145485@qq.com>
2021-05-28 18:22:03 +0800

    2021.5.28 IDEA Change

--- src/main/java/com/xztelecom/bsseteclms/BssEteClmsApplication.java
+++ src/main/java/com/xztelecom/bsseteclms/BssEteClmsApplication.java
@@ -1,5 +1,6 @@
 package com.xztelecom.bsseteclms;
 
+import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class BssEteClmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BssEteClmsApplication.class, args);
+        System.out.println("启动成功：sa-token配置如下：" + SaManager.getConfig());
     }
 
 }
