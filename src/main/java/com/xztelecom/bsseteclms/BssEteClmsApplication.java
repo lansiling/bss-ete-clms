3eeb855b8a43c677ac9f84d5d1898c6625aee635
kevin <104145485@qq.com>
2021-05-28 10:49:42 +0800

    2021.5.28

---
+++ src/main/java/com/xztelecom/bsseteclms/BssEteClmsApplication.java
@@ -0,0 +1,13 @@
+package com.xztelecom.bsseteclms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BssEteClmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BssEteClmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xztelecom/bsseteclms/BssEteClmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xztelecom.bsseteclms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BssEteClmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
